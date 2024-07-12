79bc818284c8330f6e544e9c6441b66612e3c770
igorStojanovic0 <daskalsteven117@gmail.com>
2024-07-12 22:35:52 +0900

    delete pusher

--- src/components/group/lib/pusher/client-connection.ts
+++
@@ -1,9 +0,0 @@
-import Pusher from 'pusher-js';
-
-const pusherConnection: Pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
-});
-
-const pusher = pusherConnection.subscribe('chat-app');
-
-export default pusher;
